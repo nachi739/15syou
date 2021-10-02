b78076e34e8655b9674bbf9969cf02d5d35703a2
nachi739 <namiki081025@gmail.com>
2021-10-02 20:06:06 +0900

    code15_10修正

--- 15章/src/code15_10/Main.java
+++ 15章/src/code15_10/Main.java
@@ -5,6 +5,12 @@ public class Main {
 	public static void main(String[] args) {
 		long start = System.currentTimeMillis();
 		//ここで何らかの時間がかかる処理
+		StringBuilder sb = new StringBuilder();
+		for (int i = 0; i < 10000; i++) {
+			sb.append("Java");
+		}
+		String s = sb.toString();
+		System.out.println(s);
 		long end = System.currentTimeMillis();
 		System.out.println("処理にかかった時間は…" + (end - start) + "ミリ秒でした");
 	}
